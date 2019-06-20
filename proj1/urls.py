b6987888d1457b4bb652c76b17dc2aa3c2a7e205
betwixt <bcbuenafe@gmail.com>
2019-06-20 22:37:05 +0100

    Adjust spacing on ColorCalc week bar after changes for serving React from Django

--- proj1/urls.py
+++ proj1/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^weather', include('multi_weather.urls')),
+    url(r'^weather/', include('multi_weather.urls')),
     url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
